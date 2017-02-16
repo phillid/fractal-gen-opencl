025ab484914182c39d43af5ce15e76736945e0a9
David Phillips <david@sighup.nz>
2017-02-16 23:27:29 +1300

    Create simple demo image creation script

---
+++ create-demo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+final_name="demo.png"
+temp_name="demo.tmp.pgm"
+
+clean() {
+	rm -v "$temp_name"
+}
+
+hash pnminvert || exit
+hash convert || exit
+trap clean exit
+
+echo "::: Invoking fractal-gen through pnminvert for negative PGM image"
+
+./fractal-gen -s 768 -i 75 | pnminvert > "$temp_name"
+
+echo "::: Invoking ImageMagick to convert to PNG"
+
+convert "$temp_name" "$final_name"
+
+echo "::: Success. Final image is $final_name"
